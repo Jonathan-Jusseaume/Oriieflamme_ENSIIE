e1477e1666ea8971b12f00c5c489919bf85046ae
Samy HALASSA <samy.halassa@ensiie.fr>
2022-04-29 23:05:41 +0200

    Tâche B6 : carte.h
    -ajout commentaire doxygen initialiser_carte()

--- code/headers/carte.h
+++ code/headers/carte.h
@@ -16,6 +16,12 @@ typedef struct s_carte *carte;
 
 /**
  * Fonction qui permet d'initialiser une carte
+ * @param nom : nom de la carte
+ * @param effet : effet de la carte
+ * @param effet_carte : pointeur sur fonction vers l'effet de la carte
+ * @param identifiant_faction : identifiant la faction
+ * @param numero_plateau : entier représentant la numéro de la carte sur le plateau.
+ * @param est_face_cachee : état de la carte : VRAI = face cachée, FAUX = face visible.
  * @return carte : pointeur vers la carte
  */
 carte initialiser_carte(char *nom, char *effet, void *effet_carte, int identifiant_faction, int numero_plateau,
