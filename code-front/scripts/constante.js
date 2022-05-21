c48d9fafa2774d931dbb788799f933b3293cfa6e
Michel KADILAR <michel.kadilar@ensiie.fr>
2022-05-22 00:11:10 +0200

    Lot C - Tâche C4 : Nouvelle interface web, il reste quelques détails à régler

---
+++ code-front/scripts/constante.js
@@ -0,0 +1,128 @@
+const cartes = {
+    FISE: 0,
+    FISA: 1,
+    FC: 2,
+    EcologIIE: 3,
+    lIIEns: 4,
+    Soiree_sans_alcool: 5,
+    Alcool: 6,
+    Cafe: 7,
+    The: 8,
+    Ecocup: 9,
+    Reprographie: 10,
+    Isolation_du_batiment: 11,
+    Parcours_sobriete_numerique: 12,
+    Heures_supplementaires: 13,
+    Kahina_Bouchama: 14,
+    Kevin_Goilard: 15,
+    Massinissa_Merabet: 16,
+    Vitera_Y: 17,
+    Jonas_Senizergues: 18,
+    Fetia_Bannour: 19,
+    Catherine_Dubois: 20,
+    Anne_Laure_Ligozat: 21,
+    Guillaume_Burel: 22,
+    Christophe_Mouilleron: 23,
+    Thomas_Lim: 24,
+    Julien_Forest: 25,
+    Dimitri_Watel: 26,
+    Djibril_Aurelien_Dembele_Cabot: 27,
+    Eric_Lejeune: 28,
+    Lucienne_Pacave: 29,
+    Katrin_Salhab: 30,
+    Laurent_Prevel: 31
+};
+
+let nom_cartes = [
+    "FISE",
+    "FISA",
+    "FC",
+    "EcologIIE",
+    "lIIEns",
+    "Soiree sans alcool",
+    "Alcool",
+    "Cafe",
+    "The",
+    "Ecocup",
+    "Reprographie",
+    "Isolation_du_batiment",
+    "Parcours sobriete numerique",
+    "Heures supplementaires",
+    "Kahina Bouchama",
+    "Kevin Goilard",
+    "Massinissa Merabet",
+    "Vitera Y",
+    "Jonas Senizergues",
+    "Fetia Bannour",
+    "Catherine Dubois",
+    "Anne Laure Ligozat",
+    "Guillaume Burel",
+    "Christophe Mouilleron",
+    "Thomas Lim",
+    "Julien Forest",
+    "Dimitri Watel",
+    "Djibril Aurelien Dembele Cabot",
+    "Eric Lejeune",
+    "Lucienne Pacave",
+    "Katrin Salhab",
+    "Laurent Prevel"
+];
+
+/**
+ * Tableau de chaine de caractère contenant les différents effets
+ */
+let effets_description = [
+    "La faction qui a posé cette carte gagne 1 point DDRS.",
+    "La faction qui a posé cette carte gagne 2 points DDRS si le nombre de cartes retournées sur le plateau (y compris celle-ci) est pair, et 0 sinon.",
+    "La faction qui a posé cette carte gagne 4 points DDRS si au moins une autre carte FC est retournée sur le plateau et 0 sinon",
+    "La faction qui a posé cette carte gagne 1 point DDRS par carte FISE/FISA/FC retournée.",
+    "Prenez toutes les cartes FISE/FISA/FC retournées, retirez les du plateau, mélangez les et reposez les face cachées une par une sur la gauche de la carte la plus en haut à gauche du plateau, dans cet ordre. Les prochaines cartes à être retournées sont ces cartes là.",
+    "Si au moins une carte alcool est retournée, supprimez toutes les cartes FISE/FISA/FC retournées du plateau. Supprimez ensuite la première et la dernière ligne du plateau. Sinon la faction qui a posé cette carte gagne 5 points DDRS.",
+    "Supprimez du plateau toutes les cartes qui touchent cette carte-ci (mais laissez la carte Alcool sur le plateau).",
+    "Supprimez toutes les cartes Thé et Alcool retournées sur le plateau. Si une carte Ecocup est retournée sur le plateau, la faction qui a posé cette carte gagne 1 point DDRS. Sinon elle perd 1 point DDRS.",
+    "Supprimez toutes les cartes Café et Alcool retournées sur le plateau. Si une carte Ecocup est retournée sur le plateau, la faction qui a posé cette carte gagne 1 point DDRS. Sinon elle perd 1 point DDRS.",
+    "Cette carte est sans effet.",
+    "La faction adverse de celle qui a posé cette carte perd 1 points DDRS pour chaque paire de cartes retournées et identiques sur le plateau. (S'il y a 3 cartes identiques, cela fait 3 paires).",
+    "Chaque faction gagne 1 point DDRS par carte non retournée et non supprimée du plateau qu'elle a posée sur le plateau.",
+    "Retournez toutes les cartes non retournées les plus à gauche et à droite de chaque ligne, sans appliquer leur effet.",
+    "La faction adverse de celle qui a posé cette carte perd 3 points DDRS par carte Heures supplémentaires retournée sur le plateau (y compris celle-ci).",
+    "Supprimez une carte non retournée du plateau choisie au hasard.",
+    "Supprimez une ligne au hasard, la faction qui a posé cette carte gagne 2 points DDRS par carte supprimée ainsi.",
+    "La faction qui a posé cette carte réactive l'effet de la dernière carte retournée avant Massinissa Merabet, en faisant comme elle l'avait posée elle-même, même si ce n'est pas le cas.",
+    "La faction qui a le moins de points DDRS gagne 3 points DDRS.",
+    "Supprimez toutes les cartes Heures supplémentaires retournées du plateau.",
+    "Si la carte Heures supplémentaires est retournée sur le plateau, supprimez toutes les cartes de la ligne et de la colonne où est posée cette carte (y compris celle-ci). Sinon la faction qui a posé cette carte gagne 1 point DDRS par carte Catherine Dubois, Anne-Laure Ligozat, Guillaume Burel, Christophe Mouilleron, Thomas Lim, Julien Forest et Dimitri Watel retournée sur le plateau.",
+    "Supprimez la première et la dernière cartes de la ligne et de la colonne où est posée cette carte.",
+    "Pour chaque carte EcologIIE, Ecocup, Isolation du bâtiment et parcours Sobriété numérique retournées, la faction qui a posé cette carte gagne 3 points DDRS et la dernière carte non retournée du plateau est supprimée.",
+    "Si la faction adverse de celle qui a posé cette carte a plus de points DDRS, la seconde lui vole 3 points DDRS.",
+    "Si la carte Heures supplémentaires est retournée sur le plateau, supprimez toutes les cartes retournées du plateau sauf les cartes Christophe Mouilleron et Heures supplémentaires.",
+    "Si Julien Forest n'est par retourné sur le plateau, la faction qui a posé cette carte gagne 3 points DDRS par carte FISE retournée sur le plateau. Sinon la faction adverse perd 1 point DDRS par carte FISE retournée sur le plateau.",
+    "La faction qui a posé cette carte gagne 6 points DDRS par carte FISE retournée sur le plateau si au moins une carte Café est retournée sur le plateau.",
+    "La faction qui a posé cette carte gagne 3 points DDRS par carte FISA ou FC retournée sur le plateau si au moins une carte Thé est retournée sur le plateau.",
+    "S'il y a plus de 3 cartes retournées sur la ligne de cette carte, la faction qui a posé cette carte gagne 5 points DDRS.",
+    "Prenez au hasard 5 cartes retournées du plateau (ou toutes les cartes retournées du plateau s'il y a moins de 5). Si une de ces cartes est une carte Catherine Dubois, Anne-Laure Ligozat, Guillaume Burel, Christophe Mouilleron, Thomas Lim, Julien Forest ou Dimitri Watel, mélangez les et placez les à gauche de la case la plus à gauche de la première ligne. Les prochaines cartes à être retournées sont ces cartes là. Sinon, supprimez ces cartes du plateau.",
+    "S'il y a une carte FISA retournée dans la même ligne ou la même colonne que cette carte, la faction qui a posé cette carte gagne 5 points DDRS.",
+    "Si les cartes Djibril-Aurélien Djembele-Cabeau, Eric Lejeune et Lucienne Pacavé sont retournées, la faction qui a posé cette carte gagne 10 points DDRS. Sinon, retournez toutes les cartes dans la même ligne de cette carte sans appliquer leurs effets.",
+    "Si Laurent Prével est la dernière carte retournée du plateau, la faction qui a posé cette carte gagne la manche, quel que soit le nombre de points DDRS des deux factions."
+];
+
+let chemin_images_cartes = [
+    "img/fetia.PNG"
+]
+
+let effets = new Map();
+
+Object.keys(cartes).forEach(function (key) {
+    effets.set(key, {
+        "nom": nom_cartes[cartes[key]],
+        "description": effets_description[cartes[key]],
+        "image": chemin_images_cartes[0]
+    });
+});
+console.log(effets);
+
+function getDescriptionByCardName(name) {
+    return effets.get(name.split(" ").join("_")).description;
+}
+
+console.log(getDescriptionByCardName("FISE"));
--- code-front/scripts/game.js
+++ code-front/scripts/game.js
@@ -17,12 +17,15 @@ game.addEventListener('message', function (event) {
 });
 
 
-function envoyerCarteEtPositionAPoser(indiceCarte, abscisse, ordonnee) {
-    game.send(sessionStorage.getItem("ID") + ";" + indiceCarte + ";" + abscisse + ";" + ordonnee);
+function envoyerCarteEtPositionAPoser(abscisse, ordonnee) {
+    if (previousCarteSelectedNumber !== null) {
+        game.send(sessionStorage.getItem("ID") + ";" + (previousCarteSelectedNumber + 1) + ";" + abscisse + ";" + ordonnee);
+    }
 }
 
 function envoyerDemandeRepioche(veutRemelanger) {
     game.send(sessionStorage.getItem("ID") + ";" + (veutRemelanger ? 1 : 0));
+    document.getElementById('actions').innerHTML = '';
 }
 
 // Fonction lisant le message et faisant en sorte de toujours avoir les bonnes informations sur la partie
@@ -37,26 +40,41 @@ function lireMessage(message) {
     if (message.includes("PLATEAU")) {
         // On récupère uniquement les infos, indice 0 -> grille, indice 1 -> faction 1, indice 2 -> faction 2
         const infos = (message.split("\n")).slice(1);
-
-        plateau['grille'] = parseGrille(infos[0]);
+        const splitInfosGrille = infos[0].split("/")[0].split(',');
+        plateau['grille'] = {
+            minX: parseInt(splitInfosGrille[0]),
+            minY: parseInt(splitInfosGrille[1]),
+            maxX: parseInt(splitInfosGrille[2]),
+            maxY: parseInt(splitInfosGrille[3]),
+            contenu: parseGrille(infos[0].split("/")[1])
+        };
         plateau['factions'] = [];
         plateau['factions'].push(parseFaction(infos[1]));
         plateau['factions'].push(parseFaction(infos[2]));
-        console.log(plateau);
         // lancer mise à jour de la grille
+        addMain(plateau.factions[parseInt(sessionStorage.getItem("ID"))].main);
+        MAJScoreEtManche(plateau);
+        afficherPlateau(plateau, false);
     }
     if (message.includes("PIOCHE")) {
         demander_utilisateur_repioche = true;
         // faire apparaitre le bouton pour demander à l'utilisateur de piocher
-        envoyerDemandeRepioche(false);
+        document.getElementById('actions').innerHTML =
+            "<button id='bouton-garder-pioche' class='boutons-pioche-clignote' onclick='envoyerDemandeRepioche(false)'>GARDER</button>" +
+            "<button id='bouton-repiocher' class='boutons-pioche-clignote' onclick='envoyerDemandeRepioche(true)'>MELANGER</button>";
     }
     if (message.includes("POSE")) {
         demander_utilisateur_poser_carte = true;
         // demander à un utilisateur de poser une carte
-        envoyerCarteEtPositionAPoser(1, 15, 15);
+        afficherPlateau(plateau, true);
     }
     if (message.includes("RESULTAT")) {
         resultat = parseInt(message.split(":")[1]);
+        if (resultat !== -1) {
+            document.getElementById('result').innerHTML = (resultat == sessionStorage.getItem('ID')) ? 'VICTOIRE' : 'DÉFAITE';
+        }
+
+
     }
 }
 
@@ -64,27 +82,30 @@ function parseCarte(carteString) {
     let carte = {};
     const tableau = carteString.split(",");
     carte["nom"] = tableau[0];
-    carte["position"] = {abscisse: tableau[1], ordonnee: tableau[2]};
+    carte["position"] = {abscisse: parseInt(tableau[1]), ordonnee: parseInt(tableau[2])};
     carte["identifiantFaction"] = tableau[3];
     carte["numeroPlateau"] = tableau[4];
-    carte["estFaceCachee"] = tableau[5] === 1;
+    carte["estFaceCachee"] = parseInt(tableau[5]) === 1;
     return carte;
 }
 
 function parseGrille(grilleString) {
-    const lignes = grilleString.split('|');
     const tableau = [];
-    lignes.forEach((ligne, indice) => {
-        tableau.push([]);
-        const colonnes = ligne.split(';');
-        colonnes.forEach(colonne => {
-            if (colonne !== '') {
-                tableau[indice].push(parseCarte(colonne));
-            } else {
-                tableau[indice].push(null);
-            }
+    if (grilleString != '') {
+        const lignes = grilleString.split('|');
+        lignes.forEach((ligne, indice) => {
+            tableau.push([]);
+            const colonnes = ligne.split(';');
+            colonnes.forEach(colonne => {
+                if (colonne !== '') {
+                    tableau[indice].push(parseCarte(colonne));
+                } else {
+                    tableau[indice].push(null);
+                }
+            })
         })
-    })
+    }
+
     return tableau;
 }
 
@@ -93,13 +114,185 @@ function parseFaction(factionString) {
     const tableau = factionString.split("|");
     faction["identifiant"] = tableau[0];
     faction["nom"] = tableau[1];
-    faction["aRemelanger"] = tableau[2] === 1;
+    faction["aRemelanger"] = parseInt(tableau[2]) === 1;
     faction["pointsDDRS"] = tableau[3];
     faction["manchesGagnees"] = tableau[4];
-    const main = tableau[5].split(";");
     faction["main"] = [];
-    main.forEach(carteString => {
-        faction["main"].push(parseCarte(carteString))
-    });
+    const main = tableau[5].split(";");
+    if (tableau[5] !== "") {
+        console.log(main);
+        main.forEach(carteString => {
+            faction["main"].push(parseCarte(carteString))
+        });
+    }
     return faction;
+}
+
+// TODO : (echec) Faire disparaitre la séléction d'une carte au clic autre part sur la page
+
+let previousCarteSelectedNumber = null;
+
+function rightClick(event, number) {
+    if (event.button === 2) {
+        document.getElementById('footer').getElementsByClassName("gradient-border")[number].classList.add("zoom-zoom");
+    }
+}
+
+function leftClick(currentCarteNumber) {
+    if (previousCarteSelectedNumber === null) {
+        document.getElementById('footer').getElementsByClassName("gradient-border")[currentCarteNumber].classList.add("carte-select");
+        previousCarteSelectedNumber = currentCarteNumber;
+    } else {
+        document.getElementById('footer').getElementsByClassName("gradient-border")[previousCarteSelectedNumber].classList.remove("carte-select");
+        document.getElementById('footer').getElementsByClassName("gradient-border")[currentCarteNumber].classList.add("carte-select");
+        previousCarteSelectedNumber = currentCarteNumber;
+    }
+}
+
+function rightUp(number) {
+    document.getElementById('footer').getElementsByClassName("gradient-border")[number].classList.remove("zoom-zoom");
+}
+
+/* document.addEventListener('click', function (event) {
+    for (let i = 0; i < 8; i++) {
+        if (!document.getElementsByClassName("carte")[i].contains(event.target)) {
+            document.getElementsByClassName("carte")[previousCarteSelectedNumber].classList.remove("carte-select");
+        }
+    }
+}); */
+
+function carteTemplate(carte, indice = null) {
+    let resultatHTML = "";
+    if (carte.estFaceCachee && indice == null) {
+        resultatHTML +=
+            '<div class="gradient-border">' +
+            '<div  class="carte-creation carte-creation-face-cachee">' +
+            '<div class="carte-img-face-cachee"> <img src="' + "img/logo-ensiie.png" + '"></div>' +
+            '</div>' +
+            '</div>';
+    } else {
+        resultatHTML +=
+            '<div class="gradient-border" onclick="leftClick(' + indice + ')" oncontextmenu="event.preventDefault(); return false;" onmousedown="rightClick(event,' + indice + ')" ' +
+            'onmouseup="rightUp(' + indice + ')" onmouseout="rightUp(' + indice + ')">' +
+            '<div  class="carte-creation">' +
+            '<div class="back">' +
+            '<div class="carte-title">' + carte.nom + '</div>' +
+            '<div class="carte-img-face-visible"> <img src="' + chemin_images_cartes[0] + '""></div>' +
+            '<div class="contenu-carte">' +
+            '<div class="carte-description">' + getDescriptionByCardName(carte.nom) +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+    }
+    return resultatHTML;
+}
+
+function afficherPlateau(plateau, estCliquable) {
+    previousCarteSelectedNumber = null;
+    if (plateau.grille.contenu.length && plateau.grille.contenu[0]?.length) {
+        const nombreLignes = plateau.grille.contenu.length + 2;
+        const nombreColonnes = plateau.grille.contenu[0].length + 2;
+        document.getElementById('plateau').setAttribute('style', 'grid-template-columns:' + 'repeat(' + nombreColonnes + ',194px);' + 'grid-template-rows: ' + 'repeat(' + nombreLignes + ',' + '266px);');
+        let grilleHTML = "";
+        // On ajoute une ligne en haut
+        for (let i = -1; i < nombreColonnes - 1; i++) {
+            grilleHTML += templateCaseVide(grilleHTML, {
+                abscisse: plateau.grille.minX + i,
+                ordonnee: plateau.grille.minY - 1
+            }, estCliquable);
+        }
+        plateau.grille.contenu.forEach((colonne, indice) => {
+            grilleHTML += templateCaseVide(grilleHTML, {
+                abscisse: plateau.grille.minX - 1,
+                ordonnee: plateau.grille.minY + indice
+            }, estCliquable);
+
+
+            colonne.forEach((carte, indexX) => {
+                if (carte == null) {
+                    grilleHTML += templateCaseVide(grilleHTML, {
+                        abscisse: plateau.grille.minX + indexX,
+                        ordonnee: plateau.grille.minY + indice
+                    }, estCliquable);
+                } else {
+                    grilleHTML += carteTemplate(carte);
+                }
+            })
+
+
+            grilleHTML += templateCaseVide(grilleHTML, {
+                abscisse: plateau.grille.maxX + 1,
+                ordonnee: plateau.grille.minY + indice
+            }, estCliquable);
+        });
+
+        // On ajoute une ligne en bas
+        for (let i = -1; i < nombreColonnes - 1; i++) {
+            grilleHTML += templateCaseVide(grilleHTML, {
+                abscisse: plateau.grille.minX + i,
+                ordonnee: plateau.grille.maxY + 1
+            }, estCliquable);
+        }
+
+        document.getElementById('plateau').innerHTML = grilleHTML;
+    } else {
+        document.getElementById('plateau').setAttribute('style', 'grid-template-columns:' + 'repeat(' + 1 + ',194px);' + 'grid-template-rows: ' + 'repeat(' + 1 + ',' + '266px);');
+        let grilleHTML = ""
+        if (estCliquable) {
+            grilleHTML = "<button class='pose-carte' onclick='envoyerCarteEtPositionAPoser( " + 15 + "," + 15 + ")'></button>";
+        }
+        document.getElementById('plateau').innerHTML = grilleHTML;
+    }
+}
+
+function addMain(main) {
+    let resultatHTML = "";
+    main.forEach((element, indice) => {
+        resultatHTML += carteTemplate(element, indice);
+    })
+    document.getElementsByTagName("footer")[0].innerHTML = resultatHTML;
+}
+
+function MAJScoreEtManche(plateau) {
+    let scoreFaction1 = parseInt(plateau.factions[parseInt(sessionStorage.getItem("ID"))].pointsDDRS);
+    let scoreFaction2 = parseInt(plateau.factions[(parseInt(sessionStorage.getItem("ID")) + 1) % 2].pointsDDRS);
+    let nombreManchesGagneesFaction1 = parseInt(plateau.factions[parseInt(sessionStorage.getItem("ID"))].manchesGagnees);
+    let nombreManchesGagneesFaction2 = parseInt(plateau.factions[(parseInt(sessionStorage.getItem("ID")) + 1) % 2].manchesGagnees);
+    let numeroMancheCourante = parseInt(plateau.factions[parseInt(sessionStorage.getItem("ID"))].manchesGagnees) + parseInt(plateau.factions[(parseInt(sessionStorage.getItem("ID")) + 1) % 2].manchesGagnees) + 1;
+    document.getElementById("scoreFaction1").innerHTML = scoreFaction1.toString();
+    document.getElementById("scoreFaction2").innerHTML = scoreFaction2.toString();
+    document.getElementById("nombre-manche-gagnees-faction1").innerHTML = nombreManchesGagneesFaction1.toString();
+    document.getElementById("nombre-manche-gagnees-faction2").innerHTML = nombreManchesGagneesFaction2.toString();
+    if (numeroMancheCourante < 4) {
+        document.getElementById("manche-courante").innerHTML = numeroMancheCourante.toString();
+    }
+}
+
+
+function templateCaseVide(grille, position, estCliquable) {
+    if (estCliquable && ACarteACote(plateau.grille.contenu, position)) {
+        return "<button class='pose-carte' onclick='envoyerCarteEtPositionAPoser( " + position.abscisse + "," + position.ordonnee + ")'></button>";
+    } else {
+        return "<div class='empty'></div>";
+    }
+}
+
+function ACarteACote(grille, position) {
+    for (let i = 0; i < grille.length; i++) {
+        for (let j = 0; j < grille[i].length; j++) {
+            if (grille[i][j] != null) {
+                const carte = grille[i][j];
+                if ((carte.position.abscisse - 1 === position.abscisse && carte.position.ordonnee === position.ordonnee)
+                    || (carte.position.abscisse + 1 === position.abscisse && carte.position.ordonnee === position.ordonnee)
+                    || (carte.position.abscisse === position.abscisse && carte.position.ordonnee + 1 === position.ordonnee)
+                    || (carte.position.abscisse === position.abscisse && carte.position.ordonnee - 1 === position.ordonnee)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
